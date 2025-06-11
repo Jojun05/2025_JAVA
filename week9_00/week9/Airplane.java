667e32e5f2ada13c0716d69ecf4fa5cdfcbddce8
dyd <162683643+Jojun05@users.noreply.github.com>
2025-06-11 19:00:30 +0900

    Update Airplane.java

--- week9_00/week9/Airplane.java
+++ week9_00/week9/Airplane.java
@@ -1,15 +1,19 @@
-package week9;
+package week9; // week9 패키지에 포함됨
 
+// Airplane 클래스 선언 - 비행기의 기본 동작을 정의
 public class Airplane {
-	public void land() {
-		System.out.println("착륙합니다");
-	}
+    // 착륙을 나타내는 메서드
+    public void land() {
+        System.out.println("착륙합니다"); // "착륙합니다" 출력
+    }
 
-	public void fly() {
-		System.out.println("일반비행입니다");
-	}
+    // 비행을 나타내는 메서드
+    public void fly() {
+        System.out.println("일반비행입니다"); // "일반비행입니다" 출력
+    }
 
-	public void takeoff() {
-		System.out.println("이륙합니다");
-	}
+    // 이륙을 나타내는 메서드
+    public void takeoff() {
+        System.out.println("이륙합니다"); // "이륙합니다" 출력
+    }
 }
