038c7d8985aab36d49a807e1e60d9ebbe7f3dfcd
dyd <162683643+Jojun05@users.noreply.github.com>
2025-06-11 18:52:01 +0900

    Update Student.java

--- week8/Student.java
+++ week8/Student.java
@@ -1,24 +1,36 @@
 package week8;
 
+// 학생 정보를 저장하고 처리하는 클래스
 public class Student {
 
-	private String name;
+    // 학생 이름
+    private String name;
+    // 국어 점수
     private int korean;
+    // 영어 점수
     private int english;
+    // 수학 점수
     private int math;
 
+    // 생성자: 학생 이름과 점수를 받아서 초기화
     public Student(String name, int korean, int english, int math) {
         this.name = name;
         this.korean = korean;
         this.english = english;
         this.math = math;
     }
+
+    // 총점 계산 메소드
     public int getTotal() {
         return korean + english + math;
     }
+
+    // 평균 계산 메소드
     public double getAverage() {
         return getTotal() / 3.0;
     }
+
+    // 학생 정보(이름, 총점, 평균)를 출력하는 메소드
     public void displayInfo() {
         System.out.println(name + ": 총점 " + getTotal() + "점, 평균 " + getAverage() + "점");
     }
